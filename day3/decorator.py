4df8d44bd50637374f67564fb6e3a8f756b28ccf
asd894954 <asd894954@gmail.com>
2019-07-31 14:29:47 +0300

    class work

--- day3/decorator.py
+++ day3/decorator.py
@@ -3,26 +3,27 @@ import requests
 
 
 
-def decorator( func ):
+def decorator( func  ):
 
-    def wrapper( url ):
+    def wrapper0( url ):
 
+        def wrapper1(   count ):
 
-        for i in range(10):
-            a = datetime.datetime.now()
-            func( url )
-            b = datetime.datetime.now()
+            for i in range(count):
+                a = datetime.datetime.now()
+                func( url )
+                b = datetime.datetime.now()
 
-            print(b - a)
+                print(b - a)
 
-    return wrapper 
+        return wrapper1
+    return wrapper0
 
 @decorator
 def get_url(url):
     f = requests.get(url)
 
 
-get_url("https://stackoverflow.com/questions/15138614/how-can-i-read-the-contents-of-an-url-with-python")
-
+get_url("https://stackoverflow.com/questions/15138614/how-can-i-read-the-contents-of-an-url-with-python")(5)
 
 
